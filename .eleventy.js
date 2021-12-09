84f96150c56716e47ee857574ff2e148c3e8cf8f
Jacob Lopez <jacoblopez.dev@gmail.com>
2021-12-08 19:07:21 -0600

    weather api added

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 module.exports = function(config){
 
+    config.setBrowserSyncConfig({
+        https: {
+            key: '/etc/cert/CA/localhost/localhost.decrypted.key',
+            cert: '/etc/cert/CA/localhost/localhost.crt'
+        }
+    });
+
     config.addPassthroughCopy("src/js");
+    config.addPassthroughCopy("src/css");
     return{
         dir: {
             input: "src",
