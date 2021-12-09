dd7b811db3f763dbd144577a4f79354aa589f9ca
Jacob Lopez <jacoblopez.dev@gmail.com>
2021-12-08 19:14:28 -0600

    bug fix

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 
 exports.handler = async function(event, context, callback){
     const {lat, lon } = event.queryStringParameters;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${process.env.WEATHER_API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`;
     const response = await axios.get(url);
     callback(null, {
         statusCode: 200,
         body: JSON.stringify(response.data)
     });
 
-    
+
 }
